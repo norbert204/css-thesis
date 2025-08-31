8089ce0fff5c2092e1e5a58c53ceeda7788cfc53
Horvath Norbert <nnorbert204@gmail.com>
2025-08-31 17:29:54 +0200

    Updated wording on animation property slide

--- src/Components/Slides/Animation/AnimationProperty/AnimationProperty.js
+++ src/Components/Slides/Animation/AnimationProperty/AnimationProperty.js
@@ -4,25 +4,29 @@ export default function AnimationProperty() {
     return (
         <section className='two-columns'>
             <div>
-                <h1>{ 'The ' }<code>{ 'animation' }</code>{ ' property' }</h1>
+                <h1>The <code>animation</code> property</h1>
                 <p>
-                    { 'The ' }<code>{ 'animation' }</code>{ ' is a short-hand property in CSS that let\'s you apply custom animations. ' }
-                    { 'It consists of the following sub-properties:' }
+                    The <code>animation</code> is a short-hand property that let's you apply custom made <i>animations</i> to elements.
+                    Unlike transitions, which only let you transition between two stages of a property, with animations,
+                    you can define as many stages as you need.
+                </p>
+                <p>
+                    It consists of the following sub-properties:
                 </p>
                 <ul>
-                    <li><code>{ 'animation-delay '}</code></li>
-                    <li><code>{ 'animation-direction '}</code></li>
-                    <li><code>{ 'animation-duration '}</code></li>
-                    <li><code>{ 'animation-fill-mode '}</code></li>
-                    <li><code>{ 'animation-iteration-count '}</code></li>
-                    <li><code>{ 'animation-name '}</code></li>
-                    <li><code>{ 'animation-play-state '}</code></li>
-                    <li><code>{ 'animation-timeline '}</code></li>
-                    <li><code>{ 'animation-timing-function '}</code></li>
+                    <li><code>animation-delay</code></li>
+                    <li><code>animation-direction</code></li>
+                    <li><code>animation-duration</code></li>
+                    <li><code>animation-fill-mode</code></li>
+                    <li><code>animation-iteration-count</code></li>
+                    <li><code>animation-name</code></li>
+                    <li><code>animation-play-state</code></li>
+                    <li><code>animation-timeline</code></li>
+                    <li><code>animation-timing-function</code></li>
                 </ul>
             </div>
-            <div className='centered-demo' style={ { fontSize: '5rem' } }>
-                <Loading timing={ '2s' } />
+            <div className='centered-demo' style={{ fontSize: '5rem' }}>
+                <Loading timing={'2s'} />
             </div>
         </section>
     )
