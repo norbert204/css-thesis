bd54653428d926766bd0931ed79c68a7b6e28fa9
Horvath Norbert <nnorbert204@gmail.com>
2025-09-22 20:32:11 +0200

    Made Mario example slide a bit more natural

--- src/Components/Slides/Animation/ExampleMario/ExampleMario.js
+++ src/Components/Slides/Animation/ExampleMario/ExampleMario.js
@@ -3,13 +3,13 @@ import styles from './ExampleMario.module.css'
 export default function ExampleMario() {
     return (
         <>
-            <h1>{ 'Example' }</h1>
-            <div className={ styles['container'] }>
-                <div className={ styles['mario'] }>
-                    <div className={ styles['mario-animation'] }></div>
-                    <div className={ styles['turtle-animation'] }></div>
-                    <div className={ styles['platform-1'] }></div>
-                    <div className={ styles['platform-2'] }></div>
+            <h1>Jumping Mario</h1>
+            <div className={styles['container']}>
+                <div className={styles['mario']}>
+                    <div className={styles['mario-animation']}></div>
+                    <div className={styles['turtle-animation']}></div>
+                    <div className={styles['platform-1']}></div>
+                    <div className={styles['platform-2']}></div>
                 </div>
             </div>
         </>
